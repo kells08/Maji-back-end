6bf4b272b40cd6ceecd07a764ed34b5735fd7b81
Kelly <devkells00@gmail.com>
2018-11-13 17:27:01 -0600

    cart item routes working

--- db/migrate/20181113050819_create_cart_items.rb
+++ db/migrate/20181113050819_create_cart_items.rb
@@ -2,7 +2,7 @@ class CreateCartItems < ActiveRecord::Migration[5.2]
   def change
     create_table :cart_items do |t|
       t.belongs_to :user
-      t.belongs_to :items
+      t.belongs_to :item
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_13_051511) do
+ActiveRecord::Schema.define(version: 2018_11_13_224434) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,10 +38,11 @@ ActiveRecord::Schema.define(version: 2018_11_13_051511) do
 
   create_table "cart_items", force: :cascade do |t|
     t.bigint "user_id"
-    t.bigint "items_id"
+    t.bigint "item_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["items_id"], name: "index_cart_items_on_items_id"
+    t.integer "price"
+    t.index ["item_id"], name: "index_cart_items_on_item_id"
     t.index ["user_id"], name: "index_cart_items_on_user_id"
   end
 
